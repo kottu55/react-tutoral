94143cb8735d53106a397546f8a1797323af1b69
kottu55 <kotsuboon@gmail.com>
2021-08-15 19:17:19 +0900

    stateを使った状態変化

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+import { List } from "./list.js";
 
 function App() {
+  const [description, setDescription] = useState('クリック前の表示');
+
+  const changeDescription = () => {
+    setDescription('クリック後の表示に変わりました');
+  }
   return (
     <div className="App">
-      ゼロから始めるReact入門
+      { description }
+      <List title="取り扱い言語一覧" />
+      <button onClick={changeDescription}>ボタン</button>
     </div>
   );
 }
